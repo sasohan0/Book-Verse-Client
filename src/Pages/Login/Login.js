6a691bf8774915564c01a796452babb320d6ffcb
Solih Ahmad Sohan <sasohanme@gmail.com>
2022-04-29 01:09:51 +0600

    routes added

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import auth from "../../firebase.init";
+const Login = () => {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+
+  auth.signin(username, () => {
+    // Send them back to the page they tried to visit when they were
+    // redirected to the login page. Use { replace: true } so we don't create
+    // another entry in the history stack for the login page.  This means that
+    // when they get to the protected page and click the back button, they
+    // won't end up back on the login page, which is also really nice for the
+    // user experience.
+    navigate(from, { replace: true });
+  });
+};
+
+export default Login;
